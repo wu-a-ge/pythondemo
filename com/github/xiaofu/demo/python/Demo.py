4e624ca9a76183fcd52033846f4c709aea206d64
wfygowxf <wfygowxf1@163.com>
2015-04-21 16:41:43 +0800

    python demo

--- com/github/xiaofu/demo/python/Demo.py
+++ com/github/xiaofu/demo/python/Demo.py
@@ -1,7 +1,18 @@
-#encoding=utf-8
+#encoding=gb18030
 #@author: fulaihua
-import sys
+import sys,time,os
 reload(sys)
-sys.setdefaultencoding("utf-8")
+sys.setdefaultencoding("gb18030")
+def task():
+    print "ok"
+def main():
+    while True:
+        task()
+        time.sleep(5)
+fileName=r"E:\VIP\spider\dataCrawler\QkParser1.0\QkParser1.0\output\20141428910804\tosqlserver.bat"
+try:
+    os.system(fileName)
+except Exception,e:
+    print e
 if __name__ == '__main__':
-   
+    pass
--- com/github/xiaofu/demo/python/EncodingAndDecodingTest.py
+++ com/github/xiaofu/demo/python/EncodingAndDecodingTest.py
@@ -6,28 +6,29 @@ Created on 2015��4��13��
 import unittest
 import sys
 reload(sys)
-sys.setdefaultencoding("utf-8")
+#sys.setdefaultencoding("utf-8")
 
 class Test(unittest.TestCase):
 
-
+    #此测试效果需要将sys.setdefaultencoding("utf-8")注释掉
     def testEncoding(self):
-        #这里是str字节串，用的是文件指定编码utf-8,所有u已经为str类型的字节串了
+        #这里是str字节串，用的是文件指定编码utf-8,所以u已经为str类型的字节串了
         u = '汉'
         print repr(u) # '\xe6\xb1\x89'
         #=======================================================================
         #抛异常的原因是u已经是字节串编码，
-        #所以需要先转换成unicode字符编码再使用utf-8编码成字节串编码，而由于python系统默认使用的是ascii编码，所以造成在解码的
-        #时候失败，在这种情况下需要指定系统的解码sys.setdefaultencoding("utf-8")
+        #所以需要先将u使用系统指定的编码进行解码，然后再使用指定的utf-8编码成字节串，而由于python系统默认使用的是ascii编码，所以造成在解码的
+        #时候失败，所以在这种情况下需要指定系统的解码sys.setdefaultencoding("utf-8")
         #=======================================================================
         try:
             s = u.encode('UTF-8')
         except Exception,e:
             print e
-
+    #此测试效果需要加上sys.setdefaultencoding("utf-8")
     def testDecoding(self):
         u="汉"
         print repr(u) # '\xe6\xb1\x89'
+        #u先使用系统指定的编码进行解码，再使用指定的utf-8编码
         s = u.encode('UTF-8')
         print repr(s) # '\xe6\xb1\x89'
         u2 = s.decode('UTF-8')
